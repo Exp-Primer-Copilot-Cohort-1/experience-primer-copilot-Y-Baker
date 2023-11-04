e80261569b42f977c74602d33577a6e6736637ba
Youssef Ahmed <115345602+Y-Baker@users.noreply.github.com>
2023-11-04 11:47:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React'];
+    var member = {
+        name: 'Yoon',
+        age: 27,
+        skills: skills
+    };
+    return member;
+}
